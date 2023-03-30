002bc2b12c471ad8c3a9ec52eae6573c6b4688dc
Aragorn173 <77720219+Aragorn173@users.noreply.github.com>
2023-03-30 13:43:35 +0200

    lagt in kod från tidigare som bas på modellen och tittat lite på listor

--- src/View.java
+++ src/View.java
@@ -1,8 +1,12 @@
 import javax.swing.*;
 
 public class View {
-    private JTextArea textArea1;
     private JPanel panel1;
-    private JButton button1;
-    private JButton button2;
+    private JButton createNewPostButton;
+    private JButton loginButton;
+    private JList list1;
+
+
 }
+
+
